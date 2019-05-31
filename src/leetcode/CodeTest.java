4a725a12147433f4284d5f30f4c29a44b7e88e28
wangzhiyu1212 <wangzhiyu1616@163.com>
2019-05-31 18:58:51 +0800

    0531

--- src/leetcode/CodeTest.java
+++ src/leetcode/CodeTest.java
@@ -1,18 +1,8 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class CodeTest {
 
 	public static void main(String[] args) {
-		StringBuilder s = new StringBuilder("123");
-		StringBuilder s1 = s;
-		s1.append("45");
-		String t = "123";
-		String t1 = t;
-		t1 = t1 + "45";
-		System.out.println(t);
+		
 	}
 }
---
+++ src/leetcode/RemoveNthFromEnd.java
@@ -0,0 +1,37 @@
+package leetcode;
+/**
+ * RemoveNthFromEnd r = new RemoveNthFromEnd();
+		ListNode l1 = new ListNode(2);
+    	l1.next = new ListNode(4);
+    	l1.next.next = new ListNode(3);
+    	l1.next.next.next = new ListNode(4);
+    	l1.next.next.next.next = new ListNode(5);
+    	l1.next.next.next.next.next = new ListNode(6);
+    	ListNode l2 = r.removeNthFromEnd(l1, 2);
+    	while(l2 != null) {
+    		System.out.println(l2.val);
+    		l2 = l2.next;
+    	}
+ * @author wang
+ *
+ */
+public class RemoveNthFromEnd {
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode node = dummy;
+        int l = 0;
+        while(node.next != null) {
+        	node = node.next;
+        	l++;
+        }
+        node = dummy;
+        int i = l - n ;
+        while(i>0) {
+        	node = node.next;
+        	i--;
+        }
+        node.next = node.next.next;
+        return dummy.next;
+    }
+}

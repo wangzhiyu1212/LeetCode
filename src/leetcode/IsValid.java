1be32881961378d4886e82b30df73ac750b1da23
mike <mike.wang@fwd.com>
2019-05-31 10:59:58 +0800

    20190531

---
+++ src/leetcode/IsValid.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class IsValid {
+	public boolean isValid(String s) {
+        if (s.length() == 0) return true;
+        Stack<Character> stack = new Stack<Character>();
+        for (int i=0; i<s.length(); i++) {
+        	if(s.charAt(i) == ']') {
+        		if (stack.empty() || stack.pop() != '[') return false;
+        		else continue;
+        	} else if (s.charAt(i) == ')') {
+        		if (stack.empty() || stack.pop() != '(') return false;
+        		else continue;
+        	} else if (s.charAt(i) == '}') {
+        		if (stack.empty() || stack.pop() != '{') return false;
+        		else continue;
+        	}
+        	stack.push(s.charAt(i));
+        }
+        if (stack.empty()) return true;
+        else return false;
+    }
+}

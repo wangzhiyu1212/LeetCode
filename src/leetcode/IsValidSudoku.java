b9b0cda7fea9dc0693a54c0586e2006803ccea6a
mike <mike.wang@fwd.com>
2019-06-05 10:35:54 +0800

    1

--- src/leetcode/IsValidSudoku.java
+++ src/leetcode/IsValidSudoku.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 public class IsValidSudoku {
 	public boolean isValidSudoku(char[][] board) {
         HashMap<Integer,Integer>[] rows = new HashMap[9];
-        HashMap<Integer,Integer>[] colunms = new HashMap[9];
+        HashMap<Integer,Integer>[] columns = new HashMap[9];
         HashMap<Integer,Integer>[] boxes = new HashMap[9];
         
         for (int i=0; i<9; i++) {
         	rows[i] = new HashMap<Integer,Integer>();
-        	colunms[i] = new HashMap<Integer,Integer>();
+        	columns[i] = new HashMap<Integer,Integer>();
         	boxes[i] = new HashMap<Integer,Integer>();
         }
         for (int i=0; i<9; i++) 
@@ -18,11 +18,11 @@ public class IsValidSudoku {
         		if (board[i][j] != '.') {
         			int num = (int)board[i][j];
         			rows[i].put(num, rows[i].getOrDefault(num, 0) + 1);
-        			colunms[j].put(num, colunms[j].getOrDefault(num, 0) + 1);
+        			columns[j].put(num, columns[j].getOrDefault(num, 0) + 1);
         			int box_index = (i/3)*3 + j/3;
         			boxes[box_index].put(num, boxes[box_index].getOrDefault(num, 0) + 1);
         			
-        			if(rows[i].get(num) > 1||colunms[j].get(num) > 1 ||boxes[box_index].get(num) > 1)
+        			if(rows[i].get(num) > 1||columns[j].get(num) > 1 ||boxes[box_index].get(num) > 1)
         				return false;
         		}
         	}
--- src/leetcode/SolveSudoku.java
+++ src/leetcode/SolveSudoku.java
@@ -1,8 +1,63 @@
 package leetcode;
 
 public class SolveSudoku {
+	char[][] board;
+	int n = 9;
+	int[][] rows = new int[n][n+1];
+	int[][] columns = new int[n][n+1];
+	int[][] boxes = new int[n][n+1];
+	boolean sudokuSolved = false;
+	public void placeNumber(int num,int i,int j) {
+		int index = (i/3)*3+j/3;
+		rows[i][num]++;
+		columns[j][num]++;
+		boxes[index][num]++;
+		board[i][j] = (char)(num+'0');
+	}
+	public void removeNumber(int num,int i,int j) {
+		int index = (i/3)*3+j/3;
+		rows[i][num]--;
+		columns[j][num]--;
+		boxes[index][num]--;
+		board[i][j] = '.';
+	}
+	public boolean couldPlace(int num, int row, int column) {
+		int index = (row/3)*3+column/3;
+		return rows[row][num] + columns[column][num] + boxes[index][num] == 0;
+	}
+	public void placeNextNumbers(int row, int column) {
+		if (row == n-1 && column == n-1)
+			sudokuSolved = true;
+		else {
+			if (column == n-1)
+				backtrack(row+1,0);
+			else
+				backtrack(row,column+1);
+		}
+		  
+	}
+	public void backtrack(int row,int column) {
+		if (board[row][column] == '.') {
+			for (int i=1; i<10; i++) {
+				if (couldPlace(i,row,column)) {
+					placeNumber(i,row,column);
+					placeNextNumbers(row,column);
+					if(!sudokuSolved)
+						removeNumber(i,row,column);
+				}
+			}
+		} else
+			placeNextNumbers(row,column);
+	}
 	public void solveSudoku(char[][] board) {
-		
-		
+		this.board = board;
+		for(int i=0; i<9; i++)
+			for (int j=0; j<9; j++) {
+				if (board[i][j] != '.') {
+					int num = Character.getNumericValue(board[i][j]);
+					placeNumber(num,i,j);
+				}	
+			}
+		backtrack(0,0);
     }
 }

7efe2522cc3ed6770f9f8ea677075909de98b9c0
wangzhiyu1212 <wangzhiyu1616@163.com>
2019-06-09 21:55:03 +0800

    0609

--- src/leetcode/Trap.java
+++ src/leetcode/Trap.java
@@ -4,14 +4,19 @@ import java.util.Stack;
 
 public class Trap {
 	public int trap(int[] height) {
-        int out=0;
+        int out=0,high=0,count;
         Stack<Integer> stack = new Stack<Integer>();
         for (int i=0; i<height.length; i++) {
         	while (!stack.isEmpty() && height[i] >= height[stack.peek()]) {
-        		int count = (i - stack.peek() -1) * height[stack.peek()];
-        		stack.pop();
+        		count = (height[stack.peek()] - high) * (i - stack.peek() - 1);
+        		out += count;
+        		high = height[stack.pop()];
+        	}
+        	if (!stack.isEmpty()) {
+        		count = (height[i] - high) * (i - stack.peek() - 1);
         		out += count;
         	}
+        	high = 0;
         	stack.push(i);
         }
         

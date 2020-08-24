f374d8c08dc0fccd09b5d8c1642ee2a74785b957
wangzhiyu1212 <wangzhiyu1616@163.com>
2020-08-24 11:17:07 +0800

    commit

--- src/codetest/TestTest.java
+++ src/codetest/TestTest.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class TestTest {
 
 	public static void main(String[] args) {
-		String s = "123";
-		System.out.println(s.substring(1));
+		int i = Integer.MAX_VALUE;
+		
+		System.out.println(i);
+		
 	}
 }
